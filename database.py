acecc6df6de5b47dcd1daab584949d1a4400e9ea
gefest <gefest@me.com>
2021-01-11 20:16:43 +0300

    les 3 hw

--- database.py
+++ database.py
@@ -4,27 +4,27 @@ import models
 
 
 class Database:
-
     def __init__(self, db_url):
         engine = create_engine(db_url)
         models.Base.metadata.create_all(bind=engine)
-        self.maker = sessionmaker(bind=engine)
+        self.session_m = sessionmaker(bind=engine)
 
-    def get_or_create(self, session, model, data):
-        db_model = session.query(model).filter(model.url == data['url']).first()
+    def get_or_create(self, session, model, **data):
+        db_model = session.query(model).filter(model.url == data["url"]).first()
         if not db_model:
             db_model = model(**data)
         return db_model
 
-    def create_post(self, data: dict):
-        session = self.maker()
-        tags = map(lambda tag_data: self.get_or_create(session,
-                                                       models.Tag,
-                                                       tag_data
-                                                       ), data['tags'])
-        author = self.get_or_create(session, models.Author, data['author'])
-        post = self.get_or_create(session, models.Post, data['post_data'])
-        post.author = author
+    def create_post(self, data):
+        session = self.session_m()
+        tags = map(
+            lambda tag_data: self.get_or_create(session, models.Tag, **tag_data),
+            data["tags"],
+        )
+        author = self.get_or_create(session, models.Author, **data["author"])
+        post = self.get_or_create(
+            session, models.Post, **data["post_data"], author=author
+        )
         post.tags.extend(tags)
         session.add(post)
 
@@ -34,3 +34,8 @@ class Database:
             session.rollback()
         finally:
             session.close()
+
+
+if __name__ == "__main__":
+    db = Database("sqlite:///gb_blog.db")
+    print(1)
--- gb_blog_parse.py
+++ gb_blog_parse.py
@@ -12,7 +12,6 @@ from database import Database
 
 
 class GbParse:
-
     def __init__(self, start_url, database):
         self.start_url = start_url
         self.done_urls = set()
@@ -22,7 +21,7 @@ class GbParse:
 
     def _get_soup(self, *args, **kwargs):
         response = requests.get(*args, **kwargs)
-        soup = bs4.BeautifulSoup(response.text, 'lxml')
+        soup = bs4.BeautifulSoup(response.text, "lxml")
         return soup
 
     def parse_task(self, url, callback):
@@ -39,44 +38,46 @@ class GbParse:
                 self.database.create_post(result)
 
     def post_parse(self, url, soup: bs4.BeautifulSoup) -> dict:
-        author_name_tag = soup.find('div', attrs={'itemprop': 'author'})
+        author_name_tag = soup.find("div", attrs={"itemprop": "author"})
         data = {
-            'post_data': {
-                'url': url,
-                'title': soup.find('h1', attrs={'class': 'blogpost-title'}).text,
+            "post_data": {
+                "url": url,
+                "title": soup.find("h1", attrs={"class": "blogpost-title"}).text,
             },
-            'author': {
-                'url': urljoin(url, author_name_tag.parent.get('href')),
-                'name': author_name_tag.text,
+            "author": {
+                "url": urljoin(url, author_name_tag.parent.get("href")),
+                "name": author_name_tag.text,
             },
-            'tags': [{
-                'name': tag.text,
-                'url': urljoin(url, tag.get('href')),
-            } for tag in soup.find_all('a', attrs={'class': 'small'})],
+            "tags": [
+                {
+                    "name": tag.text,
+                    "url": urljoin(url, tag.get("href")),
+                }
+                for tag in soup.find_all("a", attrs={"class": "small"})
+            ],
         }
         return data
 
     def pag_parse(self, url, soup: bs4.BeautifulSoup):
-        gb_pagination = soup.find('ul', attrs={'class': 'gb__pagination'})
-        a_tags = gb_pagination.find_all('a')
+        gb_pagination = soup.find("ul", attrs={"class": "gb__pagination"})
+        a_tags = gb_pagination.find_all("a")
         for a in a_tags:
-            pag_url = urljoin(url, a.get('href'))
+            pag_url = urljoin(url, a.get("href"))
             if pag_url not in self.done_urls:
                 task = self.parse_task(pag_url, self.pag_parse)
                 self.tasks.append(task)
                 self.done_urls.add(pag_url)
 
-        posts_urls = soup.find_all('a', attrs={'class': 'post-item__title'})
+        posts_urls = soup.find_all("a", attrs={"class": "post-item__title"})
         for post_url in posts_urls:
-            post_href = urljoin(url, post_url.get('href'))
+            post_href = urljoin(url, post_url.get("href"))
             if post_href not in self.done_urls:
                 task = self.parse_task(post_href, self.post_parse)
                 self.tasks.append(task)
                 self.done_urls.add(post_href)
 
 
-if __name__ == '__main__':
-    load_dotenv('.env')
-    parser = GbParse('https://geekbrains.ru/posts',
-                     Database(os.getenv('SQL_DB')))
+if __name__ == "__main__":
+    load_dotenv(".env")
+    parser = GbParse("https://geekbrains.ru/posts", Database(os.getenv("SQL_DB")))
     parser.run()
