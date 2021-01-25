21a40e24da44dbf4d89631f99be934a0899095f6
gefest <gefest@me.com>
2021-01-25 19:58:59 +0300

    les 6 HW

--- gb_parse/loaders.py
+++ gb_parse/loaders.py
@@ -8,17 +8,20 @@ from .items import AutoyoulaItem, HHVacancyItem
 
 def clear_price(item: str):
     try:
-        return float(item.replace("\u2009", ''))
+        return float(item.replace("\u2009", ""))
     except ValueError:
         return None
-    
+
+
 def get_author(item):
     re_str = re.compile(r"youlaId%22%2C%22([0-9|a-zA-Z]+)%22%2C%22avatar")
     result = re.findall(re_str, item)
     return urljoin("https://youla.ru/user/", result[0]) if result else None
 
+
 def get_description(items):
-    return '\n'.join(items)
+    return "\n".join(items)
+
 
 def get_specifications(data):
     tag = Selector(text=data)
@@ -26,11 +29,13 @@ def get_specifications(data):
     value = tag.xpath('//div[contains(@class, "AdvertSpecs_data")]//text()').get()
     return {name: value}
 
+
 def specifications_out(data):
     result = {}
     for itm in data:
         result.update(itm)
     return result
+
 
 class AutoyoulaLoader(ItemLoader):
     default_item_class = AutoyoulaItem
@@ -49,7 +54,7 @@ class HHVacancyLoader(ItemLoader):
     default_item_class = HHVacancyItem
     title_out = TakeFirst()
     url_out = TakeFirst()
-    description_in = ''.join
+    description_in = "".join
     description_out = TakeFirst()
-    salary_in = ''.join
+    salary_in = "".join
     salary_out = TakeFirst()
--- main.py
+++ main.py
@@ -5,11 +5,16 @@ from scrapy.settings import Settings
 from gb_parse.spiders.autoyoula import AutoyoulaSpider
 from gb_parse.spiders.instagram import InstagramSpider
 
-if __name__ == '__main__':
-    load_dotenv('.env')
+if __name__ == "__main__":
+    load_dotenv(".env")
     tasks = []
     crawler_settings = Settings()
-    crawler_settings.setmodule('gb_parse.settings')
+    crawler_settings.setmodule("gb_parse.settings")
     crawler_process = CrawlerProcess(settings=crawler_settings)
-    crawler_process.crawl(InstagramSpider, login=os.getenv('LOGIN'), password=os.getenv('ENC_PASSWORD'), tag_list=['python', 'питер', 'программирование'])
+    crawler_process.crawl(
+        InstagramSpider,
+        login=os.getenv("LOGIN"),
+        enc_password=os.getenv("ENC_PASSWORD"),
+        tag_list=["python", "питер", "программирование"],
+    )
     crawler_process.start()
